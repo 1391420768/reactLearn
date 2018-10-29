5cd9aca6faa2857402452e95422404a4e5ad925d
SanFlower <1391420768@qq.com>
2018-10-29 16:16:20 +0800

    reactTEst

---
+++ react_Test01.jsx
@@ -0,0 +1,14 @@
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>现在是 {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('example')
+  );
+}
+ 
+setInterval(tick, 1000);
